e11c49ae765e1b9d19375319753c91a4e13024d8
Ubuntu <ubuntu@ip-172-31-81-93.ec2.internal>
2019-12-07 12:38:44 +0000

    Quiz game section layout is set

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -48,4 +48,108 @@ var physicsQuestions = [{
         choices: ["Richter Scale", "Quake Scale", "Metric Scale", "Epicentre Scale"],
         answer: "Richter Scale"
     }
-]
+];
+
+var mathsQuestions = [{
+        question: "You are travelling down the motorway at 60 m/s. How long will it take you to cover a distance of 30,000 metres?",
+        choices: ["750 seconds", "50 seconds", "500 seconds", "1050 seconds"],
+        answer: "500 second"
+    },
+    {
+        question: "Convert you last answer from seconds to minutes. How many minutes does it therefore take you to travel 30,000 metres at 60m/s?",
+        choices: ["8.33 mins", "0.833 mins", "12.5 min", "17.5 mins"],
+        answer: "8.33 mins"
+    },
+    {
+        question: "Aamina and Emal Fiza are working on a maths project together, and together they are being paid a total of £10,000 in the ratio 6:4 respectively. How much does Emal get paid?",
+        choices: ["£5000", "£6000", "£4000", "£3750"],
+        answer: "£4000"
+    },
+    {
+        question: "Emal Fiza suffers from the case of a broken nose and the size of his nose increases by 2.5% each month. If his nose measured 4 cm at the beginning of December, what does his nose measure mid-April?",
+        choices: ["4.42 cm", "4.31 cm", "4.20 cm", "4.3 cm"],
+        answer: "4.42 cm"
+    },
+    {
+        question: "You are travelling in the shape of a right-handed triangle, with sides measuring - hypotenuse 50m and adjacent 43m. what is the length of the opposite side?",
+        choices: ["423 m", "754 m", "534 m", "651 m"],
+        answer: "651 m"
+    },
+    {
+        question: "How much does the angle between the given adjacent and hypotenuse in the last question measure?",
+        choices: ["50 degrees", "90 degrees", "40.7 degrees", "94.55 degrees"],
+        answer: "40.7 degrees"
+    },
+    {
+        question: "What is 567,846 rounded to the nearest 10",
+        choices: ["567,800", "567,850", "567,840", "570,000"],
+        answer: "567,850"
+    },
+    {
+        question: "Emal Fiza and Aamina are playing a quiz. For every 5 questions Aamina scores correctly, Emal scores one correct question. How many questions does Emal get correct if Aamina gets 70 questions correct?",
+        choices: ["14", "23", "11", "50"],
+        answer: "14"
+    },
+    {
+        question: "If you have a straight line 3y = 6x+5; what is the gradient of the line?",
+        choices: ["6", "2", "5", "18"],
+        answer: "2"
+    },
+    {
+        question: "What Is the y-intercept of the straight line of equation 3y = 6x+5?",
+        choices: ["3", "6", "5", "1.7"],
+        answer: "5"
+    }
+];
+
+var generalQuestions = [{
+        question: "Which chess piece is of the lowest theoretical value?",
+        choices: ["Knight", "Castle", "Bishop", "Pawn"],
+        answer: "Pawn"
+    },
+    {
+        question: "What is the capital of Canada?",
+        choices: ["Montreal", "Ottawa", "Toronto", "Vancouver"],
+        answer: "Ottawa"
+    },
+    {
+        question: "Who was the first man on the moon?",
+        choices: ["Buzz Aldrin", "Yuri Gagarin", "Neil Armstrong", "John Glennr"],
+        answer: "Neil Armstrong"
+    },
+    {
+        question: "According to the Bible, what was the name of the town which was the birthplace of Jesus Christ?",
+        choices: ["Nazareth", "Bethlehem", "Jerusalem", "Gethsemane"],
+        answer: "Bethlehem"
+    },
+    {
+        question: "What number is represented by the letters XIX in Roman numerals?",
+        choices: ["19", "16", "11", "21"],
+        answer: "19"
+    },
+    {
+        question: "Name the third known major planet from the sun in our solar system.",
+        choices: ["Venus", "Jupiter", "Mars", "Earth"],
+        answer: "Earth"
+    },
+    {
+        question: "What is the biggest living fish in the ocean?",
+        choices: ["white shark", "Whale", "Tuna", "Barracuda"],
+        answer: "Whale"
+    },
+    {
+        question: "In which African country is Timbuktu?",
+        choices: ["Algeria", "Chad", "Mali", "Nigeria"],
+        answer: "Mali"
+    },
+    {
+        question: "Name the traditional and popular wrestling sport of Japan.",
+        choices: ["Sumo", "Origami", "Ikebana", "NKendo"],
+        answer: "Sumo"
+    },
+    {
+        question: "Name the capital of Australia.",
+        choices: ["Melbourne", "Sydney", "Perth", "Canberra"],
+        answer: "Canberra"
+    }
+];
