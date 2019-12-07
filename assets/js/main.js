d2ab872581363e4ca82d3145017018d807479c97
Ubuntu <ubuntu@ip-172-31-81-93.ec2.internal>
2019-12-07 01:01:34 +0000

    Navbar and Footer is functionality is working

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+
+});
+/*
+Got help from:
+https://github.com/BlackrockDigital/startbootstrap-scrolling-nav/blob/master/js/scrolling-nav.js
+*/
+(function($) {
+  "use strict"; // Start of use strict
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+  
+})(jQuery); // End of use strict
+
+
+$(document).ready(function() {
+    $.getJSON("assets/js/test.json", function(jsonObject) {
+        console.log(jsonObject.quizTitle); // this will show the info it in firebug console
+        console.log(jsonObject.questions[0].correctAnswer); // this will show the info it in firebug console
+    });
+});
