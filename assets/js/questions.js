e4f26250eac76d2c15a7479938bda713fc3a08c7
Ubuntu <ubuntu@ip-172-31-81-93.ec2.internal>
2019-12-07 11:37:05 +0000

    Questions.js file added to save all the questions

---
+++ assets/js/questions.js
@@ -0,0 +1,51 @@
+var physicsQuestions = [{
+        question: "What is the first name of the famous scientist who gave us Newton’s three laws of motion?",
+        choices: ["Albert Einstein", "Galileo Galilei", "Michael Faraday", "Isaac Newton"],
+        answer: "Isaac Newton"
+    },
+    {
+        question: "Bolometer is used to measure?",
+        choices: ["Temperature", "Frequency", "Velocity", "Wavelength"],
+        answer: "Temperature"
+    },
+    {
+        question: "Which one of the following has the highest value of specific heat?",
+        choices: ["Alcohol", "Methane", "Water", "Kerosene"],
+        answer: "Water"
+    },
+    {
+        question: "Product of Force and Velocity is called:",
+        choices: ["Work", "Energy", "Power", "Momentum"],
+        answer: "Power"
+    },
+    {
+        question: "A man presses more weight on earth at:",
+        choices: ["leaning Position", "Lying Position", "Standing Position", "Sitting position"],
+        answer: "Standing Position"
+    },
+    {
+        question: "If electrical conductivity increases with the increase of temperature of a substance, then it is a:",
+        choices: ["Conductor", "Semiconductor", "Insulator", "Carborator"],
+        answer: "Semiconductor"
+    },
+    {
+        question: "With the increase of pressure, the boiling point of any substance",
+        choices: ["Increases", "Becomes zero", "Decreases", "Remains Same"],
+        answer: "Increases"
+    },
+    {
+        question: "The rotational effect of a force on a body about an axis of rotation is described in terms of the",
+        choices: ["Centre of gravity", "Centrifugal force", "Moment of force", "Centripetal force"],
+        answer: "Moment of force"
+    },
+    {
+        question: "What is the unit of Astronomical Distance?",
+        choices: ["Weber", "Lux", "Angstrom", "Light year"],
+        answer: "Light year"
+    },
+    {
+        question: "What is the scale used for measuring the intensity of the earthquake?",
+        choices: ["Richter Scale", "Quake Scale", "Metric Scale", "Epicentre Scale"],
+        answer: "Richter Scale"
+    }
+]
